d3bfa0282d3b5bcd32f137bbc45402867a719886
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-03 08:03:23 +0800

    add solution description

---
+++ target-sum-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    /*
+    S1 + S2 = sum 
+    S1 - S2 = target
+
+    S1*2 = sum + target
+
+    S1 = ( sum + target )/2
+    
+    if S1 -> odd : impossible 
+    */
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        
+    }
+    
+    
+};
