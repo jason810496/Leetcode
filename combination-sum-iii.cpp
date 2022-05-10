ced4fc850feb0e1c6e1279c7f98f5901f0b3f543
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-10 09:01:47 +0800

    upd version 2

--- combination-sum-iii.cpp
+++ combination-sum-iii.cpp
@@ -5,27 +5,27 @@ class Solution {
 public:
     vector<int> temp ; 
     vector< vector<int> > ans;
-    int sum = 0;
     int target;
-    void DFS(int cur ,int step){
-        if( !step || sum > target || cur == 10 ){
-            if( sum  == target && step==0 ){
-                ans.push_back( temp );
+    void DFS(int cur ,int step,int sum){
+        if( !step  ){
+            if( !sum ){
+               ans.push_back( temp );
             }
-        
             return ;
         }
-        sum+=cur;
-        temp.push_back( cur );
-        DFS(cur+1,step-1);
-        sum-=cur;
-        temp.pop_back();
-        DFS(cur+1,step);
+
+        for(int i= cur+1 ; i<=9 ; i++){
+            if( sum-i < 0 ) break;
+            
+            temp.push_back( i );
+            DFS(i , step-1 ,sum-i );
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         target = n;
 
-        DFS(1,k);
+        DFS(0,k,n);
 
         return ans;
     }
