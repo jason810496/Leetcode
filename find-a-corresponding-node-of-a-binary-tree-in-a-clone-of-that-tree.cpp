9fd939ab580461fef686cffadf19054c52a5c558
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-17 10:24:03 +0800

    AC

---
+++ find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        int T = target->val ;
+
+        queue<TreeNode*> q;
+        TreeNode* ans ; 
+        q.push(cloned);
+
+        while( q.size() ){
+            auto rt = q.front();
+            q.pop();
+
+            if( rt->val == T ){
+                ans = rt;
+                return rt;
+            }
+
+            if( rt->left ) q.push( rt->left );
+            if( rt->right ) q.push( rt->right );
+        }
+
+        return ans ;
+    }
+};
