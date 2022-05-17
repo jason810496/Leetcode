a410e1ebe23f0271c9a6235eec5827c140af4048
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-17 10:46:24 +0800

    AC by dp

--- target-sum-2.cpp
+++ target-sum-2.cpp
@@ -12,6 +12,30 @@ public:
     */
     int findTargetSumWays(vector<int>& nums, int target) {
         
+        int sum = 0 ,T , n = nums.size() ;
+        for(const int &i : nums ) sum+= i ;
+
+        T = sum+target ; 
+        if( T%2 || target > sum || target < -sum ) return 0;
+
+        T/=2;
+
+        vector< vector<int> > dp( n+1 , vector<int>(T+1) );
+
+        for(int i=0;i<n;i++) dp[i][0] = 1 ; 
+
+        for(int i=1;i<=n;i++){
+            for(int w = 0 ; w<= T ; w++){
+                if( w< nums[ i-1 ] ){
+                    dp[i][w] = dp[i-1][w];
+                }
+                else{
+                    dp[i][w] = dp[i-1][w] + dp[i-1][w - nums[i-1] ];
+                }
+            }
+        }
+
+        return dp[n][T];
         
     }
     
