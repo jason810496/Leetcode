8de7f08faf6af81ff29f55d3fc1d1ec433fcbb79
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-19 16:58:09 +0800

    change var name

--- longest-increasing-path-in-a-matrix2.cpp
+++ longest-increasing-path-in-a-matrix2.cpp
@@ -2,54 +2,61 @@
 using namespace std;
 #define pii pair<int,int>
 #define Node first 
-#define Dis second
+#define Dis second // Distance
 
 class Solution {
 public:
-    int pos[4][2]  = { {0,1},{1,0},{0,-1},{-1,0} };
-    int n , m  , N;
+    int move[4][2]  = { {0,1},{1,0},{0,-1},{-1,0} };
+    int n , m  , nm;
 
-    int longestIncreasingPath(vector<vector<int>>& Mat) {
-        n=Mat.size();
-        m=Mat[0].size();
-        N = n*m;
-        vector< vector<int> > G(N);
-        vector<int> indeg(N);
+    int longestIncreasingPath(vector<vector<int>>& Matrix) {
+        n=Matrix.size();
+        m=Matrix[0].size();
+        nm = n*m;
+
+        vector< vector<int> > Graph( nm );
+        vector<int> indeg( nm );
 
        for(int i=0;i<n;i++){
            for(int j=0;j<m;j++){
                for(int k=0;k<4;k++){
-                    int I = i+pos[k][0] , J=j+pos[k][1];
-                    if( I<0 || J<0 || I>=n || J>=m ) continue;
+                    int nxt_I = i+move[k][0] , nxt_J=j+move[k][1];
+                    if( nxt_I<0 || nxt_J<0 || nxt_I>=n || nxt_J>=m ) continue;
                     // small to big
-                    if( Mat[i][j] < Mat[I][J] ){
-                        int st = i*m+j;
-                        int ed = I*m+J;
+                    if( Matrix[i][j] < Matrix[ nxt_I][ nxt_J ] ){
+                        // turn into 1D index 
+                        int from = i*m + j ;
+                        int to = nxt_I*m + nxt_J ;
 
-                        G[st].push_back(ed);
-                        ++indeg[ed];
+                        // normal DAG
+                        Graph[ from ].push_back( to );
+                        ++indeg[ to ];
                     }
                }
            }
        } 
 
         queue< pii > q; // node , distance
+        // I have define Node as first , Dis as second
         int ans=0;
 
-        for(int i=0;i<N;i++){
+        for(int i=0;i< nm ;i++){
+            // push nodes that indeg==0 to queue 
             if( !indeg[i] ) q.push( {i,1} );
         }
 
+        // topoligical sort 
         while( q.size() ){
             pii cur = q.front() ;
             q.pop();
 
+            // update ans 
             ans = max(ans,cur.Dis);
 
-            for(int nxt : G[ cur.Node ] ){
+            for(int nxt : Graph[ cur.Node ] ){ 
                 if( nxt == cur.Node ) continue;
 
-                if( !--indeg[nxt] ){
+                if( !--indeg[nxt] ){ // trick 
                     q.push( { nxt ,cur.Dis+1 } );
                 }
             }
